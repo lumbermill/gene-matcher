71e6bc81abd635484775acbe4c0ca1ffb75fabb5
ITO Yosei <y-itou@lumber-mill.co.jp>
2023-09-12 05:48:20 +0900

    released 0.1.8, Matcher's target_sequence is now optional.

--- gene-matcher.gemspec
+++ gene-matcher.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'gene-matcher'
-  s.version     = '0.1.7'
-  s.date        = '2023-09-11'
+  s.version     = '0.1.8'
+  s.date        = '2023-09-12'
   s.summary     = "Algorithm for determining similar regions between nucleic acid sequences."
   s.description = "Algorithm for determining similar regions between nucleic acid sequences."
   s.authors     = ["ITO Yosei"]
--- lib/gene-matcher.rb
+++ lib/gene-matcher.rb
@@ -2,11 +2,12 @@
 require_relative 'smith-waterman'
 
 class Matcher
-  attr_accessor :input_sequence, :limit
+  attr_accessor :input_sequence, :limit, :reserve_target_sequence
   attr_reader :alignments
 
   def initialize(input_sequence, limit = 0.6)
     @limit = limit
+    @reserve_target_sequence = false
     @input_sequence = input_sequence
     @alignments = []
   end
@@ -14,7 +15,8 @@ class Matcher
   def scan(target_sequence,source = {})
     sw = SmithWaterman.instance
     a = sw.alignment(target_sequence, @input_sequence)
-    a.source = {target_sequence: target_sequence}.merge(source)
+    a.source = {}.merge(source)
+    a.source[:target_sequence] = target_sequence if @reserve_target_sequence
     puts "#{a.score} / #{a.alignment_count} = #{a.score / a.alignment_count}" if ENV["DEBUG"]
     @alignments += [a] if a.score / a.alignment_count >= @limit
   end
